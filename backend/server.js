4526750ea44ea647ee0ab2faece19971f942ac23
Bishal Rana <rbishal50@gmail.com>
2023-06-15 13:43:33 +0545

    [Api] Add starter server file and starter scripts on the root

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import products from "./data/products.js";
+
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API IS RUNNING...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
