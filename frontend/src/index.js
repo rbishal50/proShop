75d1b1d12700c607fb01cb54ec449a4f388d9110
Bishal Rana <rbishal50@gmail.com>
2023-06-29 13:13:27 +0545

    [App] Add a shipping details screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ShippingScreen from "./screens/ShippingScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />}></Route>
       <Route path="/login" element={<LoginScreen />}></Route>
       <Route path="/register" element={<RegisterScreen />}></Route>
+      <Route path="/shipping" element={<ShippingScreen />}></Route>
     </Route>
   )
 );
