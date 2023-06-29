56578fb2776ee5f5a0eddea7bbc1f951071becdd
Bishal Rana <rbishal50@gmail.com>
2023-06-29 13:20:03 +0545

    [App] Add private route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,16 +18,19 @@ import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />}></Route>
-      <Route path="/product/:id" element={<ProductScreen />}></Route>
-      <Route path="/cart" element={<CartScreen />}></Route>
-      <Route path="/login" element={<LoginScreen />}></Route>
-      <Route path="/register" element={<RegisterScreen />}></Route>
-      <Route path="/shipping" element={<ShippingScreen />}></Route>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
