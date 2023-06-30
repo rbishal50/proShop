4e2bda5aabe1e71c46acf5db5b41a80a56b41cd0
Bishal Rana <rbishal50@gmail.com>
2023-06-30 14:34:40 +0545

    [App] Clean unused items in order screen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,27 +1,12 @@
 import { Link, useParams } from "react-router-dom";
-import {
-  Row,
-  Col,
-  ListGroup,
-  Image,
-  Form,
-  Button,
-  Card,
-} from "react-bootstrap";
+import { Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
 
 const OrderScreen = () => {
   const { id: orderId } = useParams();
-  const {
-    data: order,
-    refetch,
-    isLoading,
-    error,
-  } = useGetOrderDetailsQuery(orderId);
-
-  console.log(order);
+  const { data: order, isLoading, error } = useGetOrderDetailsQuery(orderId);
 
   if (isLoading) return <Loader />;
 
