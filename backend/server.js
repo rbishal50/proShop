c26c6ef39f21c83f4c44ae587d3007d78e182b61
Bishal Rana <rbishal50@gmail.com>
2023-07-02 16:01:50 +0545

    [Api] Add a webhook url for paypal

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,9 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
+app.post("/api/webhook/paypal", (req, res) => {
+  // res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
 
 app.use(notFound);
 app.use(errorHandler);
