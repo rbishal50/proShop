2c5ef04f4285e3ae38df27dd32f246571baff465
Bishal Rana <rbishal50@gmail.com>
2023-07-07 13:18:14 +0545

    [App/Api] Add orders list screen and controllers for admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -91,7 +91,8 @@ const updateOrderToDelivered = asyncHandler(async function (req, res) {
 // @route GET /api/orders
 // @access Admin/ Private
 const getOrders = asyncHandler(async function (req, res) {
-  res.send("get all orders");
+  const orders = await Order.find({}).populate("user", "id name");
+  res.status(200).json(orders);
 });
 
 export {
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -35,6 +35,13 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    // admins
+    getOrders: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -44,4 +51,5 @@ export const {
   usePayOrderMutation,
   useGetPaypalClientIdQuery,
   useGetMyOrdersQuery,
+  useGetOrdersQuery,
 } = ordersApiSlice;
