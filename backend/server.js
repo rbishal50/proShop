07618b42628198d6349d1c73d8a8441ae5cbd2e3
Bishal Rana <rbishal50@gmail.com>
2023-07-17 13:24:17 +0545

    [Api] Add build command

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API IS RUNNING...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -40,6 +36,20 @@ app.post("/api/webhook/paypal", (req, res) => {
 
 const __dirname = path.resolve(); // Set __dirname to root directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (_req, res) => {
+    res.send("API IS RUNNING...");
+  });
+}
 
 app.use(notFound);
 
